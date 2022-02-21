f45ec7ac32f86a2cd5c59900d96b45d64a429e7e
dior <90284737+DIORrios285@users.noreply.github.com>
2022-02-22 00:46:28 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ from config import (
     APP_ID,
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
-    FORCE_SUB_CHANNEL1,
     FORCE_SUB_GROUP,
     LOGGER,
     OWNER,
@@ -51,22 +50,6 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).info(
                     "\nBot Berhenti. Gabung Group https://t.me/fandasupport untuk Bantuan"
-                )
-                sys.exit()
-        if FORCE_SUB_CHANNEL1:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
-                self.invitelink3 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_CHANNEL1!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_GROUP:
